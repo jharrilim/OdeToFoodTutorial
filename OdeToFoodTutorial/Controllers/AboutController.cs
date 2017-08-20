d90fd08f69f5d4c048cf75afe01ca9dcfc92a735
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2017-08-20 12:50:23 -0400

    Module 4.4

---
+++ OdeToFoodTutorial/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFoodTutorial.Controllers
+{
+    public class AboutController
+    {
+		public string Phone()
+		{
+			return "123-456-7890";
+		}
+
+		public string Address()
+		{
+			return "1 Yonge Street";
+		}
+    }
+}
--- OdeToFoodTutorial/Startup.cs
+++ OdeToFoodTutorial/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
 
 namespace OdeToFoodTutorial
 {
@@ -27,22 +29,38 @@ namespace OdeToFoodTutorial
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
 			services.AddSingleton(Configuration);
 			services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+        public void Configure
+			(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
         {
+			loggerFactory.AddConsole();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+			else
+			{
+				app.UseExceptionHandler(new ExceptionHandlerOptions()
+				{
+					ExceptionHandler = context => context.Response.WriteAsync("Whoops")
+				});
+			}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync(greeter.GetGreeting());
-            });
+			app.UseFileServer();
+
+			app.UseMvc(ConfigureRoutes);
+
+			app.Run(ctx => ctx.Response.WriteAsync("Not Found"));
         }
-    }
+
+		private void ConfigureRoutes(IRouteBuilder routeBuilder)
+		{
+			routeBuilder.MapRoute("Default","{controller=Home}/{action=Index}/{id?}");
+		}
+	}
 }
