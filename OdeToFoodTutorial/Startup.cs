844abe79341c4a3f801441bf1666aeb2888ac4c8
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2017-08-20 11:29:17 -0400

    ASP.NET Core Fundamentals - Module 2

--- OdeToFoodTutorial/Startup.cs
+++ OdeToFoodTutorial/Startup.cs
@@ -6,19 +6,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFoodTutorial
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+		public IConfiguration Configuration { get; set; }
+
+		public Startup(IHostingEnvironment environment)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(environment.ContentRootPath)
+				.AddJsonFile("appsettings.json")
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+        
+		// This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddSingleton(Configuration);
+			services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +41,7 @@ namespace OdeToFoodTutorial
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(greeter.GetGreeting());
             });
         }
     }
