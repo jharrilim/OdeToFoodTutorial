367808be8a03f02eb703fba0154d25ba52cf46e6
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2017-08-20 13:57:20 -0400

    Module 5.2 - View Models, Entities

--- OdeToFoodTutorial/Controllers/HomeController.cs
+++ OdeToFoodTutorial/Controllers/HomeController.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using OdeToFoodTutorial.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFoodTutorial.Services;
+using OdeToFoodTutorial.ViewModels;
 
 namespace OdeToFoodTutorial.Controllers
 {
     public class HomeController : Controller
     {
 		private IRestaurantData _restaurantData;
+		private IGreeter _greeter;
 
-		public HomeController(IRestaurantData data)
+		public HomeController(IRestaurantData data, IGreeter greeter)
 		{
 			_restaurantData = data;
+			_greeter = greeter;
 		}
 
 		public IActionResult Index()
 		{
-			var model = _restaurantData.GetAllRestaurants();
+			var model = new HomePageViewModel();
+			model.Restaurants = _restaurantData.GetAllRestaurants();
+			model.Motd = _greeter.GetGreeting();
 			return View(model);
 		}
     }
---
+++ OdeToFoodTutorial/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using OdeToFoodTutorial.Entities;
+using System.Collections.Generic;
+
+namespace OdeToFoodTutorial.ViewModels
+{
+    public class HomePageViewModel
+    {
+		public string Motd { get; set; }
+		public IEnumerable<Restaurant> Restaurants { get; set; }
+
+	}
+}
