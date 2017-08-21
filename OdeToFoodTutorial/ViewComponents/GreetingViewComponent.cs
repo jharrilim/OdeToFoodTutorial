60e5517180f04b0f4af8d8dd63fab5eadc4c2425
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2017-08-20 21:12:31 -0400

    Module 7.8 - View Components

---
+++ OdeToFoodTutorial/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFoodTutorial.Services;
+using System.Threading.Tasks;
+
+namespace OdeToFoodTutorial.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+		private IGreeter _greeter;
+
+		public GreetingViewComponent(IGreeter greeter)
+		{
+			_greeter = greeter;
+		}
+
+		public Task<IViewComponentResult> InvokeAsync()
+		{
+			var model = _greeter.GetGreeting();
+			return Task.FromResult<IViewComponentResult>(View("Default", model));
+		}
+    }
+}
